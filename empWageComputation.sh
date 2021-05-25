2c655f71725463b72511447f64b101bc4b665234
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 00:31:48 +0530

    Add : calculating wage for 100hrs or 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -x
 wagePerHr=20
-for((i=1;i<=20;i++))
+while [ $((RANDOM%1)) ]
 do
-	randomCheck=$((RANDOM%3))
-	case $randomCheck in
-			1) workingHrs=8
-				;;
-			2) workingHrs=4
-				;;
-			3) workingHrs=0
-				;;
-	esac
-	salary=$(($salary+$(($wagePerHr*$workingHrs))))
+randomCheck=$((RANDOM%3))
+case $randomCheck in
+		1) workingHrs=8
+			;;
+		2) workingHrs=4
+			;;
+		3) workingHrs=0
+			;;
+esac
+salary=$(($salary+$(($wagePerHr*$workingHrs))))
+hrs=$(($hrs+$workingHrs))
+day=$(($day+1))
+if [[ $hrs -eq 100 || $day -eq 20 ]]
+then
+	echo "Employee's salary is : $salary"
+	break
+fi
 done
-echo "Employee salary for 20 days is : $salary "
