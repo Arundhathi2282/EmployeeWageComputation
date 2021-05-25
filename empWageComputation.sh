a428fac2eda777ec552ac6e644b02d2521735f3b
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-25 22:11:34 +0530

    Add : calculating fulltime present employee salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
 isPresent=1
+wagePerHr=20
 randomCheck=$((RANDOM%2))
 if [[ $randomCheck -eq $isPresent ]]
 then
 	echo "Employee is Present"
+	fullDayHr=8
 else
 	echo "Employee is Absent"
 fi
+salary=$(($wagePerHr*$fullDayHr))
+echo "Employee is full time present and his salary is : $salary"
