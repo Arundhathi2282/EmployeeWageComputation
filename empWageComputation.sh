3d07445073508ea32e26273cbd0a90f169ecd092
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 02:04:37 +0530

    Add : Storing daily wage along with Total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,20 @@
 #!/bin/bash -x
-function Workinghours()
-{
-while [ $((RANDOM%1)) ]
-do
-randomCheck=$((RANDOM%3))
-case $randomCheck in
-		1) workingHrs=8
-			;;
-		2) workingHrs=4
-			;;
-		3) workingHrs=0
-			;;
-esac
-hrs=$(($hrs+$workingHrs))
-day=$(($day+1))
-if [[ $hrs -eq 100 || $day -eq 20 ]]
-then
-	echo "Working Hours : $hrs "
-	break
-fi
-done
-}
 wagePerHr=20
-Workinghours
+declare -a array
+for((i=1;i<=20;i++))
+do
+	randomCheck=$((RANDOM%3))
+	case $randomCheck in
+			1) workingHrs=8
+				;;
+			2) workingHrs=4
+				;;
+			*) workingHrs=0
+				;;
+	esac
+	salary1=$(($wagePerHr*$workingHrs))
+	array[$i]=$salary1
+	salary=$(($salary+$salary1))
+done
+echo ${array[@]}
+echo "Total wage is : $salary"
