ea1b371f483a7dc83e7ca2b432cb9f46ac8bd290
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-25 22:36:53 +0530

    Add : Calculating employee salary using switchcase

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,15 @@
 #!/bin/bash -x
-echo "Welcome to Employee Wage Computation"
-isPresent=1
-isParttime=2
 wagePerHr=20
 randomCheck=$((RANDOM%3))
-if [[ $randomCheck -eq $isPresent ]]
-then
-	echo "Employee is Present"
-	workingHrs=8
-elif [[ $randomCheck -eq $isParttime ]]
-then
-	echo "Employee is Parttime Present"
-	workingHrs=8
-else
-	echo "Employee is Absent"
-	workingHrs=0
-fi
+case $randomCheck in
+		1) echo "Employee is Full Time Present"
+		   workingHrs=8
+			;;
+		2) echo "Employee is Part Time Present"
+		   workingHrs=4
+			;;
+		*) echo "Employee is Absent"
+		   workingHrs=0
+			;;
+esac
 salary=$(($wagePerHr*$workingHrs))
