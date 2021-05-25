9a67650f9927da69ef6316eefcaa9e1b4ae6e36c
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-25 23:14:49 +0530

    Add : Employee Wage for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
 wagePerHr=20
-randomCheck=$((RANDOM%3))
-case $randomCheck in
-		1) echo "Employee is Full Time Present"
-		   workingHrs=8
-			;;
-		2) echo "Employee is Part Time Present"
-		   workingHrs=4
-			;;
-		*) echo "Employee is Absent"
-		   workingHrs=0
-			;;
-esac
-salary=$(($wagePerHr*$workingHrs))
+for((i=1;i<=20;i++))
+do
+	randomCheck=$((RANDOM%3))
+	case $randomCheck in
+			1) workingHrs=8
+				;;
+			2) workingHrs=4
+				;;
+			3) workingHrs=0
+				;;
+	esac
+	salary=$(($salary+$(($wagePerHr*$workingHrs))))
+done
+echo "Employee salary for 20 days is : $salary "
