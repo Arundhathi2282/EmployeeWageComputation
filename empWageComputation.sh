2f053f9935504efafc81cbd37c7b1a9a33da7365
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-25 22:26:11 +0530

    Add : calculating parttime salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
 isPresent=1
+isParttime=2
 wagePerHr=20
-randomCheck=$((RANDOM%2))
+randomCheck=$((RANDOM%3))
 if [[ $randomCheck -eq $isPresent ]]
 then
 	echo "Employee is Present"
-	fullDayHr=8
+	workingHrs=8
+elif [[ $randomCheck -eq $isParttime ]]
+then
+	echo "Employee is Parttime Present"
+	workingHrs=8
 else
 	echo "Employee is Absent"
+	workingHrs=0
 fi
-salary=$(($wagePerHr*$fullDayHr))
-echo "Employee is full time present and his salary is : $salary"
+salary=$(($wagePerHr*$workingHrs))
