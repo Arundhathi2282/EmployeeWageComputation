d2d033032e8392dca39ee93a32ffc5bbfaf0ac93
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 02:26:27 +0530

    Add : day, dailywage along with totalwage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -x
 wagePerHr=20
 declare -a array
-for((i=1;i<=20;i++))
+read -p "Enter a num : " num
+for((i=1;i<=$num;i++))
 do
-	randomCheck=$((RANDOM%3))
-	case $randomCheck in
-			1) workingHrs=8
-				;;
-			2) workingHrs=4
-				;;
-			*) workingHrs=0
-				;;
-	esac
-	salary1=$(($wagePerHr*$workingHrs))
-	array[$i]=$salary1
-	salary=$(($salary+$salary1))
+        randomCheck=$((RANDOM%3))
+        case $randomCheck in
+                        1) workingHrs=8
+                                ;;
+                        2) workingHrs=4
+                                ;;
+                        *) workingHrs=0
+                                ;;
+        esac
+        salary1=$(($wagePerHr*$workingHrs))
+        array[$i]=$salary1
+        salary=$(($salary+$salary1))
 done
 echo ${array[@]}
+for i in ${!array[@]}
+do
+        echo "Day $i = ${array[$i]}"
+done
 echo "Total wage is : $salary"
+
 
